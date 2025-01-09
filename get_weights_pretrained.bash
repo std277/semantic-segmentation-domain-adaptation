8b56798fab0d8ff872853bfcc223de677eba4bfa
Borella Simone <borellasimone00@gmail.com>
2025-01-10 00:03:28 +0100

    Update

--- get_weights_pretrained.bash
+++ get_weights_pretrained.bash
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-# Create data directory if it doesn't exist
+# Create weights_pretrained directory if it doesn't exist
 mkdir -p weights_pretrained
 
-wget https://drive.google.com/file/d/1ZX0UCXvJwqd2uBGCX7LI2n-DfMg3t74v/view?usp=sharing
-
-mv deeplab_resnet_pretrained_imagenet.pth weights_pretrained/deeplab_resnet_pretrained_imagenet.pth
+gdown "https://drive.google.com/uc?id=1ZX0UCXvJwqd2uBGCX7LI2n-DfMg3t74v" -O "weights_pretrained/deeplab_resnet_pretrained_imagenet.pth"
